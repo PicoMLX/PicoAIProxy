b8318e6853f3ac4e62bfd86fda5a0e38d16799c8
Ronald Mannak <ronaldmannak@me.com>
2024-01-19 14:50:02 -0800

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "1.10.1"),
-        .package(url: "https://github.com/hummingbird-project/hummingbird-auth", from: "1.3"),
+        .package(url: "https://github.com/hummingbird-project/hummingbird-auth", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.19.0"),
         .package(url: "https://github.com/apple/app-store-server-library-swift.git", from: "1.0.1"),
@@ -25,7 +25,6 @@ let package = Package(
                 .product(name: "ArgumentParser", package: "swift-argument-parser"), 
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "AppStoreServerLibrary", package: "app-store-server-library-swift"),
-                .product(name: "AppAttest", package: "AppAttest"),
                 .product(name: "JWTKit", package: "jwt-kit"),
             ],
             resources: [
